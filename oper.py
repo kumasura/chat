2836e3bbc38d91f4da13b1df9fd21142c9220b55
kumasura <suraj.kumar.2013@gmail.com>
2025-08-08 12:49:07 +0530

    Create oper.py

---
+++ oper.py
@@ -0,0 +1,24 @@
+import modal
+import os
+import wget
+import shutil
+
+
+app = modal.App()
+volume = modal.Volume.from_name("elabs-phi-verse", create_if_missing=True)
+
+@app.function(volumes={"/myvol": volume})
+@app.local_entrypoint()
+def main():
+    #g.remote()
+    url = 'https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_0.gguf'
+    file_Path = 'mistral2.gguf'
+    wget.download(url, file_Path)
+    with volume.batch_upload() as batch:
+        batch.put_file("mistral2.gguf", "/mistral2.gguf")
+    
+
+    
+    
+    
+    
