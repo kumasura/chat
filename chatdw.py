525944c5c44c9f7669dab85d70c757fa5b9f21ee
kumasura <suraj.kumar.2013@gmail.com>
2025-09-22 07:32:33 +0530

    123

--- chatdw.py
+++ chatdw.py
@@ -7,7 +7,11 @@ volume = modal.Volume.from_name("elabs-phi-verse", create_if_missing=True)
 outlines_image = (
     Image.from_registry(
         "python:3.13-alpine3.21"
-    ).apt_install("git")
+    )
+    .run_commands(
+        "apt-get update",
+        "DEBIAN_FRONTEND=noninteractive apt-get install -y git",
+    )
     .pip_install(
         "torch", 
         "git+https://github.com/huggingface/transformers accelerate",
